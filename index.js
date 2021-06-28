f3b53f975071af6c9144d6261c93b578d6f1775d
Julia Johnson <julialj95@gmail.com>
2021-06-27 21:23:37 -0500

    refactored render code to avoid repetition

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+let postsArray = [];
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((data) => {
-    const postsArr = data.slice(0, 5);
-    console.log(postsArr);
-    const postsDiv = document.getElementById("posts");
-    const posts = postsArr.map(
-      (post) =>
-        `<h2 class="post-title">${post.title}</h2><p class="post-body">${post.body}</p><hr />`
-    );
-    postsDiv.innerHTML = posts;
+    console.log(data);
+    postsArray = data.slice(0, 5);
+    console.log("postsArray", postsArray);
+    renderPosts();
   });
 
-document.getElementById("post-button").addEventListener("click", createPost);
+function renderPosts() {
+  let posts = postsArray.map((post) => {
+    return `<h2 class="post-title">${post.title}</h2><p class="post-body">${post.body}</p><hr />`;
+  });
+  document.getElementById("posts").innerHTML = posts;
+}
 
-function createPost(e) {
+document.getElementById("post-button").addEventListener("click", function (e) {
   e.preventDefault();
   const postTitle = document.getElementById("title").value;
   const postBody = document.getElementById("body").value;
@@ -30,5 +33,8 @@ function createPost(e) {
     },
   })
     .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+    .then((post) => {
+      postsArray.unshift({ title: post.title, body: post.body });
+      renderPosts();
+    });
+});
